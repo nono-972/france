6132caf06434ace2d5b5beecc587db98c3d23c2d
Noham <mnoham972@gmail.comgit config --global user.email mnoham972@gmail.comgit config --global user.email mnoham972@gmail.com>
2026-05-02 19:58:33 +0200

    equipes

---
+++ asset/javascript/noham.js
@@ -0,0 +1,9 @@
+let section = document.createElement("section")
+let main = document.querySelector("main")
+
+main.appendChild(section)
+
+section.textContent = "Notre agence peut vous offrir, plus qu'un voyage et plus que des vacances. On peux vous offrir le voyage de vos rêve et dans les meilleurs conditions possibles"
+
+let size = 20
+
