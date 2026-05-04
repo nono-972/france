77e5e9fb942b440c143b24e90081369939b9ae83
Noham <mnoham972@gmail.comgit config --global user.email mnoham972@gmail.comgit config --global user.email mnoham972@gmail.com>
2026-05-04 17:36:30 +0200

    hexagone

---
+++ asset/javascript/script.js
@@ -0,0 +1,19 @@
+let h2 = document.createElement("h2")
+h2.classList.add("creole")
+let nav = document.querySelector("nav")
+
+nav.appendChild(h2)
+
+let span = document.createElement("span")
+span.classList.add('color')  
+span.textContent = "Nou la pou"
+
+
+span.classList.add('forêt')
+span.textContent = "fè rèv zot"
+
+
+span.classList.add('Bèlè')
+span.textContent = "rivé"
+
+h2.appendChild (span) 
