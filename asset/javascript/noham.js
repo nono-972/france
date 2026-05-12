ad10ce8bde36b20c38634502e6b319698b4c7be3
Noham <mnoham972@gmail.comgit config --global user.email mnoham972@gmail.comgit config --global user.email mnoham972@gmail.com>
2026-05-12 16:45:42 +0200

    exploiter

--- asset/javascript/noham.js
+++ asset/javascript/noham.js
@@ -3,7 +3,7 @@ let main = document.querySelector("main")
 
 main.appendChild(section)
 
-section.textContent = "Notre agence peut vous offrir, plus qu'un voyage et plus que des vacances. On peux vous offrir le voyage de vos rêve et dans les meilleurs conditions possibles"
+section.textContent = "Notre agence peut vous offrir, plus qu'un voyage et plus que des vacances. On peux vous offrir le voyage de vos rêve et dans les meilleurs conditions possibles."
 
 let h5 = document.createElement("h5")
 h5.classList.add("creole")
