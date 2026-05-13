5fd7dacb6541face09309c94cd85122e8ce708e9
Noham <mnoham972@gmail.comgit config --global user.email mnoham972@gmail.comgit config --global user.email mnoham972@gmail.com>
2026-05-13 21:17:46 +0200

    louer

--- asset/javascript/paris2.js
+++ asset/javascript/paris2.js
@@ -3,9 +3,9 @@ let a = document.querySelector('svg')
 a.style.marginRight = 10
 a.style.marginLeft = 5
 
-let span = document.getElementsById('samuraÏ')
+let span = document.getElementById('samuraÏ')
 
-samuraÏ.style.marginLeft = 10
+samuraÏ.style.marginLeft = "10"+"px"
 
 let h5 = document.createElement("h5")
 h5.classList.add("creole")
@@ -13,3 +13,8 @@ let nav = document.querySelector("nav")
 
 nav.appendChild(h5)
 h5.textContent = " Nous sommes là pour réaliser vos rêves "
+
+let h2 = document.querySelector('h2')
+
+h2.style.paddingTop = " 5rem "
+h2.style.paddingBottom = "5rem"
--- asset/javascript/script.js
+++ asset/javascript/script.js
@@ -5,3 +5,6 @@ let nav = document.querySelector("nav")
 nav.appendChild(h5)
 h5.textContent = " Nous sommes là pour réaliser vos rêves " 
 
+let p = document.querySelector('p')
+
+p.style.marginRight = " 8rem "
